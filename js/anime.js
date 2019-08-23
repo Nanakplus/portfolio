14fa9046d7bd9aa3fe5c4f365bb1c24ae46791ed
Nanakplus <49552812+Nanakplus@users.noreply.github.com>
2019-08-24 05:54:05 +0900

    js

---
+++ js/anime.js
@@ -0,0 +1,10 @@
+bubbly({
+    colorStart: "#252525",
+    colorStop: "#111111",
+    blur: 1,
+    bubbles: 180,
+    compose: "source-over",
+    shadowColor: "#145bd4",
+    radiusFunc: () => Math.random() * 15,
+    bubbleFunc: () => `hsla(0, 0%, 80%, ${Math.random() * 0.25})`,
+});
